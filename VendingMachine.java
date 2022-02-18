af2d4b1b172ede29b9000e05fcf1f3b08d4bded7
Horacio Valdes <horavaldesg@gmail.com>
2022-02-18 17:16:15 -0500

    Welcome message with instructions

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,8 +1,14 @@
-import java.Util.Scanner;
+// import java.util.Scanner;
 public class VendingMachine {
     public static void main (String[] args){
         String[] snacks = {"Chips", "Soda", "Candy", "Pretzel", "Skittles"};
         float[] price = {1.00f, 1.25f, .50f, 1.50f, .75f};
 
+        System.out.print("Welcome! The Snacks available are: ");
+        for(int i = 0; i < snacks.length; i++){
+            System.out.print(i + 1 + ") " + snacks[i] + " ");
+        }
+
+        
     }
 }
