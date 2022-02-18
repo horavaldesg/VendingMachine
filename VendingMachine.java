c8a5ae6d2b0f4448d3b515dd474841ba4ef6703f
Mario Egocheaga <43833659+Mario-Egocheaga@users.noreply.github.com>
2022-02-18 16:58:56 -0500

    prices

--- VendingMachine.java
+++ VendingMachine.java
@@ -2,6 +2,7 @@ import java.Util.Scanner;
 public class VendingMachine {
     public static void main (String[] args){
         String[] snacks = {"Chips", "Soda", "Candy", "Pretzel", "Skittles"};
+        float[] price = {1.00f, 1.25f, .50f, 1.50f, .75f};
 
     }
 }
