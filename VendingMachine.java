a813df19f9476d658ada6734a115775dd5c0b981
Horacio Valdes <horavaldesg@gmail.com>
2022-02-18 16:54:16 -0500

    Added variety of snacks

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,3 +1,7 @@
+import java.Util.Scanner;
 public class VendingMachine {
-    
+    public static void main (String[] args){
+        String[] snacks = {"Chips", "Soda", "Candy", "Pretzel", "Skittles"};
+
+    }
 }
