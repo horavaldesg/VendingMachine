e2859229e6f20ae1ccd41f700976a07d4a1f7adf
Horacio Valdes <horavaldesg@gmail.com>
2022-02-18 18:24:35 -0500

    Transaction

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,10 +1,19 @@
  import java.util.Scanner;
 public class VendingMachine {
     public static void main (String[] args){
+        //Horacio
         String[] snacks = {"Chips", "Soda", "Candy", "Pretzel", "Skittles"};
-        float[] price = {1.00f, 1.25f, .50f, 1.50f, .75f};
-        Scanner kb = new Scanner(System.in);
 
+        float[] price = {1.00f, 1.25f, .50f, 1.50f, .75f};
+
+        Scanner kb = new Scanner(System.in);
+        
+        //Horacio
+        float moneyBalance = 0;
+        int moneyInserted = 0;
+        float originalBalance = 0;
+
+        Boolean doneInsteringMoney = false;
         System.out.print("Welcome! The Snacks available are: ");
         for(int i = 0; i < snacks.length; i++){
             System.out.print(i + 1 + ") " + snacks[i] + " ");
@@ -27,15 +36,13 @@ public class VendingMachine {
             break;
             case 5: System.out.println("User Selected: " + snacks[choice - 1] + " Price: " + price[choice - 1]);
             break;
-            default: System.out.println("Not an Option");
+            default: System.out.println("Not an Option!");
+            System.exit(0);
             break;
 
         }
 
-        float moneyBalance = 0;
-        int moneyInserted = 0;
-        
-        Boolean doneInsteringMoney = false;
+        //Horacio
         while(doneInsteringMoney == false){
             
             System.out.println("How much money would you like to insert? (Only One Dollar Bills Accepted)");
@@ -47,28 +54,49 @@ public class VendingMachine {
             }
             else if(moneyInserted == 1){
                
-            moneyBalance += moneyInserted;
-       
-            System.out.println("Money Available: " + moneyBalance);
-            System.out.println("Would you like to stop inserting money? If YES press 1 if NO Press 2");
-            System.out.print("> ");
-            int userOption = kb.nextInt();
-                if(userOption == 1){
-                    doneInsteringMoney = true;
-                }
-                else if(userOption == 2){
-                    doneInsteringMoney = false;
-                }
-            }
-            if(moneyBalance > price[choice - 1])
-            {
-            	float change = 0;
-            	change = moneyBalance - price[choice - 1];
-            	System.out.println("Your Change is : " + change);
-            	
+                moneyBalance += moneyInserted;
+        
+                System.out.println("Money Available: " + moneyBalance);
+                System.out.println("Would you like to stop inserting money? If YES press 1 if NO Press 2");
+                System.out.print("> ");
+                int userOption = kb.nextInt();
+                    if(userOption == 1){
+                        doneInsteringMoney = true;
+                        originalBalance = moneyBalance;
+                    }
+                    else if(userOption == 2){
+                        doneInsteringMoney = false;
+                        originalBalance = moneyBalance;
+                    }
             }
            
+            }
+            //Horacio
+        System.out.println("Would you like to purchase? If yes press 1 if you would like a refund press 2");
+        switch(kb.nextInt()){
+            case 1:
+            
+            if(moneyBalance >= price[choice -1]){
+                if(moneyBalance > price[choice - 1])
+                {
+                    float change = 0;
+                    change = moneyBalance - price[choice - 1];
+                    System.out.println("Your Change is : $" + change);
+                }
+                System.out.println("Purchase Complete!");
+            }
+            else{
+                System.out.println("Not Enough Money!");
+            }
+            break;
+            case 2:
+            System.out.println("Purchase Canceled! Refunded for: $" + originalBalance);
+            break;
+            default: System.out.println("Not an Option!");
+            break;
         }
+            
+        kb.close();
         
     }
 }
