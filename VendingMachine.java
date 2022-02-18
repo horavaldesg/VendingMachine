ee41bd99a56a8ee78c599d6f5ae7f48282951b4e
Mario Egocheaga <43833659+Mario-Egocheaga@users.noreply.github.com>
2022-02-18 17:24:41 -0500

    User choice

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,14 +1,34 @@
-// import java.util.Scanner;
+ import java.util.Scanner;
 public class VendingMachine {
     public static void main (String[] args){
         String[] snacks = {"Chips", "Soda", "Candy", "Pretzel", "Skittles"};
         float[] price = {1.00f, 1.25f, .50f, 1.50f, .75f};
+        Scanner kb = new Scanner(System.in);
 
         System.out.print("Welcome! The Snacks available are: ");
         for(int i = 0; i < snacks.length; i++){
             System.out.print(i + 1 + ") " + snacks[i] + " ");
         }
 
+        int choice = 0;
+
+        choice = kb.nextInt();
+        switch (choice)
+        {
+            case 1: System.out.println("User Selected: " + snacks[choice - 1] + " Price: " + price[choice - 1]);
+            break;
+            case 2: System.out.println("User Selected: " + snacks[choice - 1] + " Price: " + price[choice - 1]);
+            break;
+            case 3: System.out.println("User Selected: " + snacks[choice - 1] + " Price: " + price[choice - 1]);
+            break;
+            case 4: System.out.println("User Selected: " + snacks[choice - 1] + " Price: " + price[choice - 1]);
+            break;
+            case 5: System.out.println("User Selected: " + snacks[choice - 1] + " Price: " + price[choice - 1]);
+            break;
+            default: System.out.println("Not an Option");
+            break;
+);
+        }
         
     }
 }
